841a9fb97bf65fedce0b2e6ee43328a66d60cceb
Raycoms <ray.neiheiser@gmail.com>
2017-04-02 17:13:06 -0300

    reformat

--- src/main/java/main/java/com/bag/main/DatabaseLoader.java
+++ src/main/java/main/java/com/bag/main/DatabaseLoader.java
@@ -15,29 +15,46 @@ import org.jetbrains.annotations.NotNull;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.HashSet;
-import java.util.List;
-import java.util.logging.Logger;
 
 /**
- * Created by daniel on 01/04/17.
+ * Loads databases with file stream.
  */
-public class DatabaseLoader {
+public class DatabaseLoader
+{
 
-    private IDatabaseAccess dbAcces;
-    private String graphLocation;
-    private boolean idAsLabel;
-    public static final String LABEL = "BAGNode";
+    /**
+     * The database access object.
+     */
+    private final IDatabaseAccess dbAccess;
 
-    public DatabaseLoader(IDatabaseAccess dbAcces, String graphLocation, boolean idAsLabel) {
-        this.dbAcces = dbAcces;
+    /**
+     * Location of the graph.
+     */
+    private final String          graphLocation;
+
+    /**
+     * Should the id become label or default label?
+     */
+    private final boolean         idAsLabel;
+
+    /**
+     * Static label used for BAg.
+     */
+    private static final String LABEL = "BAGNode";
+
+    public DatabaseLoader(IDatabaseAccess dbAcces, String graphLocation, boolean idAsLabel)
+    {
+        this.dbAccess = dbAcces;
         this.graphLocation = graphLocation;
         this.idAsLabel = idAsLabel;
     }
 
-    public void loadGraph() throws Throwable {
+    public void loadGraph() throws Throwable
+    {
         HashSet<Integer> cache = new HashSet<>();
 
-        try(FileReader fr = new FileReader(graphLocation); BufferedReader br = new BufferedReader(fr);) {
+        try (FileReader fr = new FileReader(graphLocation); BufferedReader br = new BufferedReader(fr);)
+        {
             String line;
             int nodeOperations = 0;
             int relOperations = 0;
@@ -46,10 +63,13 @@ public class DatabaseLoader {
             int count = 0;
             long nanos = System.nanoTime();
             long totalNanos = nanos;
-            while ((line = br.readLine()) != null) {
+            while ((line = br.readLine()) != null)
+            {
                 String[] fields = line.split(" ");
                 if (fields.length < 3)
+                {
                     continue;
+                }
 
                 int origin = Integer.parseInt(fields[0]);
                 int destination = Integer.parseInt(fields[2]);
@@ -57,38 +77,41 @@ public class DatabaseLoader {
                 NodeStorage nodeOrigin = createNode(fields[0]);
                 NodeStorage nodeDest = createNode(fields[2]);
 
-                if (!cache.contains(origin)) {
-                    dbAcces.applyCreate(nodeOrigin, 1);
+                if (!cache.contains(origin))
+                {
+                    dbAccess.applyCreate(nodeOrigin, 1);
                     nodeOperations += 1;
                     count += 1;
                     cache.add(origin);
                 }
 
-                if (!cache.contains(destination)) {
-                    dbAcces.applyCreate(nodeDest, 1);
+                if (!cache.contains(destination))
+                {
+                    dbAccess.applyCreate(nodeDest, 1);
                     nodeOperations += 1;
                     count += 1;
                     cache.add(destination);
                 }
 
                 RelationshipStorage rel = new RelationshipStorage(fields[1], nodeOrigin, nodeDest);
-                dbAcces.applyCreate(rel, 1);
+                dbAccess.applyCreate(rel, 1);
 
-                /*List<Object> lst = dbAcces.readObject(rel, 1);
+                /*List<Object> lst = dbAccess.readObject(rel, 1);
                 for (Object l : lst)
                     System.out.printf("%s\n", l.toString());
 
-                lst = dbAcces.readObject(nodeDest, 1);
+                lst = dbAccess.readObject(nodeDest, 1);
                 for (Object l : lst)
                     System.out.printf("%s\n", l.toString());*/
 
                 relOperations += 1;
                 count += 1;
 
-                if (count >= 1000) {
+                if (count >= 1000)
+                {
                     count = 0;
                     double dif = (System.nanoTime() - nanos) / 1000000000.0;
-                    System.out.printf("Time: %.3f s\nNodes: %d (%d new)\nRelations: %d (%d new)\n\n", dif, nodeOperations,
+                    System.out.printf("Time: %.3f s%nNodes: %d (%d new)%nRelations: %d (%d new)%n%n", dif, nodeOperations,
                             nodeOperations - lastNodeOperations, relOperations, relOperations - lastRelOperations);
                     lastNodeOperations = nodeOperations;
                     lastRelOperations = relOperations;
@@ -97,24 +120,33 @@ public class DatabaseLoader {
             }
 
             double dif = (System.nanoTime() - totalNanos) / 1000000000.0;
-            System.out.printf("---------FINISHED--------\nTotal Time: %.3f s\nNodes: %d (%d new)\nRelations: %d (%d new)\n\n", dif, nodeOperations,
+            System.out.printf("---------FINISHED--------%nTotal Time: %.3f s%nNodes: %d (%d new)%nRelations: %d (%d new)%n%n", dif, nodeOperations,
                     nodeOperations - lastNodeOperations, relOperations, relOperations - lastRelOperations);
         }
     }
 
-    private NodeStorage createNode(String id) {
-        if (idAsLabel) {
-            NodeStorage result = new NodeStorage(id);
-            return result;
+    private NodeStorage createNode(String id)
+    {
+        if (idAsLabel)
+        {
+            return new NodeStorage(id);
         }
-        else {
+        else
+        {
             NodeStorage result = new NodeStorage(LABEL);
             result.addProperty("Id", id);
             return result;
         }
     }
 
-    public static IDatabaseAccess instantiateDBAccess(@NotNull final String instance, final int globalServerId)
+    /**
+     * Instantiate the database access.
+     * @param instance the instance to use.
+     * @param globalServerId the global server id (used to find the folder)
+     * @return the access object.
+     */
+    @NotNull
+    private static IDatabaseAccess instantiateDBAccess(@NotNull final String instance, final int globalServerId)
     {
         switch (instance)
         {
@@ -123,39 +155,46 @@ public class DatabaseLoader {
             case Constants.TITAN:
                 return new TitanDatabaseAccess(globalServerId);
             case Constants.SPARKSEE:
-                return  new SparkseeDatabaseAccess(globalServerId);
+                return new SparkseeDatabaseAccess(globalServerId);
             case Constants.ORIENTDB:
                 return new OrientDBDatabaseAccess(globalServerId);
             default:
-                Log.getLogger().warn("Invalid databaseAccess");
+                Log.getLogger().warn("Invalid databaseAccess - default to Neo4j.");
+                return new Neo4jDatabaseAccess(globalServerId);
         }
-        return null;
     }
 
-
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         String databaseId = "neo4";
         boolean idAsLabel = true;
         if (args.length > 0)
+        {
             databaseId = args[0];
+        }
         if (args.length > 1)
+        {
             idAsLabel = Boolean.parseBoolean(args[1]);
+        }
 
 
         Log.getLogger().setLevel(Level.WARN);
 
         IDatabaseAccess access = instantiateDBAccess(databaseId, 0);
-        System.out.printf("Starting %s database\n", databaseId);
+        System.out.printf("Starting %s database%n", databaseId);
         access.start();
         System.out.printf("Loading...");
 
         DatabaseLoader loader = new DatabaseLoader(access, "/home/daniel/ray/thesis/src/testGraphs/social-a-graph.txt", idAsLabel);
-        try {
+        try
+        {
             loader.loadGraph();
-        } catch (Throwable throwable) {
+        }
+        catch (Throwable throwable)
+        {
             throwable.printStackTrace();
         }
-        System.out.printf("Closing %s database\n", databaseId);
+        System.out.printf("Closing %s database%n", databaseId);
         access.terminate();
     }
 }
