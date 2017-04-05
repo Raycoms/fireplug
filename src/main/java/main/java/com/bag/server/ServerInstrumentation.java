c6030e4853576d5a4e1d3233e923f73280109990
Daniel Presser <Daniel Presser>
2017-04-05 16:21:44 -0300

    Show minutes elapsed

--- src/main/java/main/java/com/bag/server/ServerInstrumentation.java
+++ src/main/java/main/java/com/bag/server/ServerInstrumentation.java
@@ -48,9 +48,15 @@ public class ServerInstrumentation {
      */
     private int id;
 
+    /**
+     * Minutes elapsed since the start.
+     */
+    private int minutesElapsed;
+
     public ServerInstrumentation(int id) {
         this.id = id;
         lastCommit = System.nanoTime();
+        minutesElapsed = 0;
     }
 
     public void updateCounts(int writes, int reads, int commits, int aborts) {
@@ -68,6 +74,7 @@ public class ServerInstrumentation {
                 double elapsed = ((System.nanoTime() - lastCommit) / Constants.NANO_TIME_DIVIDER);
                 if (elapsed >= 60.0) {
                     lastCommit = System.nanoTime();
+                    minutesElapsed += 1;
 
                     try(final FileWriter file = new FileWriter(System.getProperty("user.home") + "/results"+id+".txt", true);
                         final BufferedWriter bw = new BufferedWriter(file);
@@ -81,8 +88,8 @@ public class ServerInstrumentation {
                         out.print(readsPerformed.get() + writesPerformed.get() + ";");
                         out.println();
 
-                        System.out.println(String.format("Elapsed: %.3f s\nAborted: %d\nCommited: %d\nReads: %d\nWrites: %d\nThroughput: %d\n\n",
-                                elapsed, abortedTransactions.get(), committedTransactions.get(), readsPerformed.get(),
+                        System.out.println(String.format("Elapsed: %.3fs (%d minutes)\nAborted: %d\nCommited: %d\nReads: %d\nWrites: %d\nThroughput: %d\n\n",
+                                elapsed, minutesElapsed, abortedTransactions.get(), committedTransactions.get(), readsPerformed.get(),
                                 writesPerformed.get(), readsPerformed.get() + writesPerformed.get()));
 
                         abortedTransactions = new AtomicInteger(0);
