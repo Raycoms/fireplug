053f63d833eed80cbc4412d6f9ef32534bf7140c
Raycoms <ray.neiheiser@gmail.com>
2017-04-07 15:45:15 -0300

    add new parameter to multiClientRunner
    for amount of servers he has to contact

--- src/main/java/main/java/com/bag/main/MultipleClientRunner.java
+++ src/main/java/main/java/com/bag/main/MultipleClientRunner.java
@@ -6,61 +6,77 @@ import java.util.List;
 import java.util.Random;
 
 /**
- * Created by daniel on 02/04/17.
+ * Runs multiple clients at the same time.
  */
-public class MultipleClientRunner {
+public class MultipleClientRunner
+{
 
-    public class OutputPrinter extends Thread {
+    public class OutputPrinter extends Thread
+    {
         private InputStream stream;
-        private String clientName;
+        private String      clientName;
 
-        public OutputPrinter(String clientName, InputStream stream) {
+        public OutputPrinter(String clientName, InputStream stream)
+        {
             this.stream = stream;
-            this.clientName =clientName;
+            this.clientName = clientName;
         }
 
         @Override
-        public void run() {
-            try {
+        public void run()
+        {
+            try
+            {
                 BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
                 String line;
-                while ((line = reader.readLine()) != null) {
+                while ((line = reader.readLine()) != null)
+                {
                     System.out.println(clientName + ": " + line);
                 }
             }
-            catch (IOException e) {
+            catch (IOException e)
+            {
                 e.printStackTrace();
             }
         }
     }
 
-    public MultipleClientRunner() {
+    public MultipleClientRunner()
+    {
 
     }
 
-    public void runClients(String option, int initialClient, int finalClient, int totalClients, String addresses) {
-        try {
+    public void runClients(final String option, final int initialClient, final int finalClient, final int totalClients, final int amountsOfServers, final String addresses)
+    {
+        try
+        {
             Random rnd = new Random();
             System.out.printf("Starting...\n");
             List<Process> procs = new ArrayList<Process>();
             String[] directAddresses = new String[0];
             if (addresses != null)
+            {
                 directAddresses = addresses.split(",");
+            }
 
-            for (int i = initialClient; i <= finalClient; i++) {
-                int serverPartner = i % 3;
+            for (int i = initialClient; i <= finalClient; i++)
+            {
+                int serverPartner = i % amountsOfServers;
                 String cmd;
-                if (option.equals("bag")) {
+                if (option.equals("bag"))
+                {
                     cmd = String.format("java -cp build/libs/1.0-0.1-Setup-fat.jar main.java.com.bag.main.RunTests true %d -1 1 %d %d false 2",
                             serverPartner, totalClients, i);
                 }
-                else if (option.equals("direct")) {
+                else if (option.equals("direct"))
+                {
                     serverPartner = i % directAddresses.length;
                     String[] address = directAddresses[serverPartner].split(":");
                     cmd = String.format("java -cp build/libs/1.0-0.1-Setup-fat.jar main.java.com.bag.main.RunTests false %s %s 1 1 %d false 2",
                             address[0], address[1], i);
                 }
-                else {
+                else
+                {
                     System.out.println("Invalid option " + option);
                     return;
                 }
@@ -78,25 +94,32 @@ public class MultipleClientRunner {
             }
 
             for (Process p : procs)
+            {
                 p.waitFor();
+            }
 
             System.out.printf("Finishing...\n");
         }
-        catch (Throwable t) {
+        catch (Throwable t)
+        {
             t.printStackTrace();
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         String opt = args[0];
         String address = null;
         int initialClient = Integer.parseInt(args[1]);
         int finalClient = Integer.parseInt(args[2]);
         int totalClients = Integer.parseInt(args[3]);
-        if (args.length > 4)
-            address = args[4];
+        int amountOfServers = Integer.parseInt(args[4]);
+        if (args.length > 5)
+        {
+            address = args[5];
+        }
 
         MultipleClientRunner runner = new MultipleClientRunner();
-        runner.runClients(opt, initialClient, finalClient, totalClients, address);
+        runner.runClients(opt, initialClient, finalClient, totalClients, amountOfServers, address);
     }
 }
