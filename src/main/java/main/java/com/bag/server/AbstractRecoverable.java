b9c25d1f4b9b7743eaf4ab06e3546326ca02004f
Raycoms <ray.neiheiser@gmail.com>
2017-05-24 17:14:37 -0300

    make storage reaaaaly big

--- src/main/java/main/java/com/bag/server/AbstractRecoverable.java
+++ src/main/java/main/java/com/bag/server/AbstractRecoverable.java
@@ -243,7 +243,7 @@ public abstract class AbstractRecoverable extends DefaultRecoverable
         final KryoPool pool = new KryoPool.Builder(factory).softReferences().build();
         final Kryo kryo = pool.borrow();
 
-        Output output = new Output(0, 800240);
+        Output output = new Output(0, 1000240);
         kryo.writeObject(output, getGlobalSnapshotId());
 
         for (final Map.Entry<Long, List<IOperation>> writeSet : globalWriteSet.entrySet())
