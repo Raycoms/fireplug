4c05e3733afddd534627c32ea4c5debf86943d3e
Raycoms <ray.neiheiser@gmail.com>
2017-07-19 18:23:45 +0100

    improve sparksee config, add logging

--- src/main/java/main/java/com/bag/util/HashCreator.java
+++ src/main/java/main/java/com/bag/util/HashCreator.java
@@ -23,12 +23,13 @@ public class HashCreator
 
         byte[] result = mDigest.digest(node.getBytes());
 
-        StringBuilder sb = new StringBuilder();
+        final StringBuilder sb = new StringBuilder();
         for (final byte aResult : result)
         {
             sb.append(Integer.toString((aResult & 0xff) + 0x100, 16).substring(1));
         }
 
+        Log.getLogger().warn(sb.toString());
         return sb.toString();
     }
 
