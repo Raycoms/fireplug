96e38fed95a9462640ce60350bac6d8590ba0da8
Raycoms <ray.neiheiser@gmail.com>
2017-10-04 19:07:31 +0100

    a bit more neo4j again

--- src/main/java/main/java/com/bag/client/TestClient.java
+++ src/main/java/main/java/com/bag/client/TestClient.java
@@ -677,7 +677,7 @@ public class TestClient extends ServiceProxy implements BAGClient, ReplyReceiver
 
         int randomNumber = random.nextInt(100000);
 
-        if(randomNumber <= 70000)
+        if(randomNumber <= 72500)
         {
             serverProcess = 0;
             return;
