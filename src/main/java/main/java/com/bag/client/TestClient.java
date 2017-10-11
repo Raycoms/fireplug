77630d7e7f9e71d64481cc7e6e4d4ade0b27dc12
Raycoms <ray.neiheiser@gmail.com>
2017-10-11 18:57:18 +0100

    arg

--- src/main/java/main/java/com/bag/client/TestClient.java
+++ src/main/java/main/java/com/bag/client/TestClient.java
@@ -678,7 +678,7 @@ public class TestClient extends ServiceProxy implements BAGClient, ReplyReceiver
         int randomNumber = random.nextInt(100);
 
 
-        if(randomNumber <= 70)
+        if(randomNumber <= 50)
         {
             serverProcess = 0;
             return;
