0f9d373a3351868b7399e954e0472770ba1a788a
Raycoms <ray.neiheiser@gmail.com>
2017-10-12 16:32:43 +0100

    invoke unordered

--- src/main/java/main/java/com/bag/client/TestClient.java
+++ src/main/java/main/java/com/bag/client/TestClient.java
@@ -477,7 +477,7 @@ public class TestClient extends ServiceProxy implements BAGClient, ReplyReceiver
             }
             else
             {
-                answer = globalProxy.invokeOrdered(bytes);
+                answer = invokeUnordered(bytes);
             }
 
             Log.getLogger().info(getProcessId() + "Committed with snapshotId " + this.localTimestamp);
