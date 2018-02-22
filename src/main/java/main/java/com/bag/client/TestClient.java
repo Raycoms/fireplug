e2e4bbf02fe6038927a2cd5f4cffb631f1e3ea90
Raycoms <ray.neiheiser@gmail.com>
2018-02-22 15:04:53 +0000

    turn off local

--- src/main/java/main/java/com/bag/client/TestClient.java
+++ src/main/java/main/java/com/bag/client/TestClient.java
@@ -511,7 +511,7 @@ public class TestClient implements BAGClient, ReplyListener
             else
             {
                 //Do it in optimistic mode in local cluster (if >= 4 replicas)
-                if(localProxy.getViewManager().getCurrentViewProcesses().length >= 4)
+                if(localProxy.getViewManager().getCurrentViewProcesses().length >= 4 && false)
                 {
                     Log.getLogger().info("To Local proxy:");
                     answer = localProxy.invokeUnordered(bytes);
