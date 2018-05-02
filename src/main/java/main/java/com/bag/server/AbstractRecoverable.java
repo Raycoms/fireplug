9518882e8dcafbbd4692a5a88d4d44ffd3d128c9
Raycoms <ray.neiheiser@gmail.com>
2018-05-02 21:08:57 +0100

    more threads

--- src/main/java/main/java/com/bag/server/AbstractRecoverable.java
+++ src/main/java/main/java/com/bag/server/AbstractRecoverable.java
@@ -114,7 +114,7 @@ public abstract class AbstractRecoverable extends DefaultRecoverable
     /**
      * Thread pool for message sending.
      */
-    protected final ExecutorService service = Executors.newFixedThreadPool(3);
+    protected final ExecutorService service = Executors.newFixedThreadPool(10);
 
     /**
      * Thread pool for message sending.
