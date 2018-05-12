c17945bb592ef8af8e96da32051e2b8ea8505b80
Raycoms <ray.neiheiser@gmail.com>
2018-05-12 14:39:00 +0100

    add some logging

--- src/main/java/main/java/com/bag/server/GlobalClusterSlave.java
+++ src/main/java/main/java/com/bag/server/GlobalClusterSlave.java
@@ -474,6 +474,7 @@ public class GlobalClusterSlave extends AbstractRecoverable
 
                 if (wrapper.getLocalCluster().getId() == 0 || wrapper.getLocalCluster().isPrimarySubstitute())
                 {
+                    Log.getLogger().warn("Got enough signatures, got: " + signatureStorage.getSignatures().size() + " signatures!");
                     final DistributeMessageThread runnable = new DistributeMessageThread(messageOutput.getBuffer());
                     service.submit(runnable);
                 }
@@ -884,6 +885,7 @@ public class GlobalClusterSlave extends AbstractRecoverable
 
                 if (wrapper.getLocalCluster().getId() == 0 || wrapper.getLocalCluster().isPrimarySubstitute())
                 {
+                    Log.getLogger().warn("Got enough signatures, got: " + signatureStorage.getSignatures().size() + " signatures!");
                     final DistributeMessageThread runnable = new DistributeMessageThread(messageOutput.getBuffer());
                     service.submit(runnable);
                 }
