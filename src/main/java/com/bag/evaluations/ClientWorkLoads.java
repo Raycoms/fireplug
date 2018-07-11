8890caf0b2b8be0b4f760a0808d150c9bf040971
Raycoms <ray.neiheiser@gmail.com>
2018-07-11 14:49:08 -0300

    longer timeout

--- src/main/java/com/bag/evaluations/ClientWorkLoads.java
+++ src/main/java/com/bag/evaluations/ClientWorkLoads.java
@@ -463,7 +463,7 @@ public class ClientWorkLoads
                 RelationshipStorage readRelationship = null;
                 NodeStorage readNodeStorage = null;
                 IOperation operation = null;
-                if (commits > 250)
+                if (commits > 350)
                 {
                     if (isRead && random.nextInt(2) > 0)
                     {
@@ -474,7 +474,7 @@ public class ClientWorkLoads
                         isRead = false;
                     }
                 }
-                else if (commits == 250)
+                else if (commits == 350)
                 {
                     client.turnUpWrites();
                 }
