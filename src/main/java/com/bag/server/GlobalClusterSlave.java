3913d601c7f7e348dcfc1685ac693a3684436911
Raycoms <ray.neiheiser@gmail.com>
2018-07-12 21:42:07 -0300

    5min

--- src/main/java/com/bag/server/GlobalClusterSlave.java
+++ src/main/java/com/bag/server/GlobalClusterSlave.java
@@ -261,7 +261,7 @@ public class GlobalClusterSlave extends AbstractRecoverable
                 Log.getLogger().warn("Start reconfiguration manager!");
                 Log.getLogger().warn("-----------------------------------");
                 reconfigurationManager = new ReconfigurationManager(this, proxy.getViewManager().getCurrentViewN());
-                timer.scheduleAtFixedRate(reconfigurationManager, 2 * 60000, 10000);
+                timer.scheduleAtFixedRate(reconfigurationManager, 5 * 60000, 10000);
             }
         }
 
